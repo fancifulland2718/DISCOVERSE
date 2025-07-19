48236042a556de17cb2711217492c606e1bf64bd
jyf23 <1305116641@qq.com>
2025-07-19 11:26:14 +0800

    v1.8.5

--- discoverse/__init__.py
+++ discoverse/__init__.py
@@ -6,3 +6,12 @@ if os.getenv('DISCOVERSE_ASSETS_DIR'):
     print(f'>>> get env "DISCOVERSE_ASSETS_DIR": {DISCOVERSE_ASSETS_DIR}')
 else:
     DISCOVERSE_ASSETS_DIR = os.path.join(DISCOVERSE_ROOT_DIR, 'models')
+
+__version__ = "1.8.5"
+__logo__ = """
+    ____  _________ __________ _    ____________  _____ ______
+   / __ \/  _/ ___// ____/ __ \ |  / / ____/ __ \/ ___// ____/
+  / / / // / \__ \/ /   / / / / | / / __/ / /_/ /\__ \/ __/   
+ / /_/ // / ___/ / /___/ /_/ /| |/ / /___/ __ _/___/ / /___   
+/_____/___//____/\____/\____/ |___/_____/_/ |_|/____/_____/   
+"""
